d68eb18c9d9153184671adfc1d97bc3f8dcbf524
rauleb <rauleburro@gmail.com>
2016-08-03 11:25:25 +0200

    -Generamos los archivos para genetica en la parte del frontend

---
+++ app/scripts/controllers/genetica.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name frontendmuApp.controller:GeneticaCtrl
+ * @description
+ * # GeneticaCtrl
+ * Controller of the frontendmuApp
+ */
+angular.module('frontendmuApp')
+  .controller('GeneticaCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/services/genetica.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name frontendmuApp.genetica
+ * @description
+ * # genetica
+ * Service in the frontendmuApp.
+ */
+angular.module('frontendmuApp')
+  .service('genetica', function () {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+  });
